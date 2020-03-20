989a4e89b0f606603b32648dcbafc4a3c84e2749
Iain Watts <iain.watts@gmail.com>
2020-03-19 21:29:08 -0700

    simulate_equity_from_game_spec function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 mod cards;
 use cards::Card;
+mod equity;
 mod game;
 use game::GameSpec;
-use game::Game;
 mod hands;
-
-const NUM_SIMULATIONS: u64 = 1000000;
 
 fn main() {
     let mut board = Vec::new();
@@ -17,22 +15,15 @@ fn main() {
     hole_cards.push(Some( (Card::from_str("Qh"), Card::from_str("Qd")) ));
     hole_cards.push(Some( (Card::from_str("Ac"), Card::from_str("As")) ));
     let game_spec = GameSpec { board, hole_cards };
-
-    let mut player_win_counts: Vec<u64> = vec![0, 0];
-
-    for _ in 0..NUM_SIMULATIONS {
-        let mut game = Game::from_spec(&game_spec);
-        game.deal_down_to_river();
-        let winning_players_and_hands = game.get_winning_players_and_hands();
-        if winning_players_and_hands.len() == 1 {
-            let (player, _) = winning_players_and_hands.iter().next().unwrap();
-            player_win_counts[*player] += 1;
-        }
+    
+    let results = equity::simulate_equity_from_game_spec(game_spec, None);
+    let zipped_percs = results.win_percentages.iter().zip(results.draw_percentages.iter());
+    for (player, (win_perc, draw_perc)) in zipped_percs.enumerate() {
+        println!(
+            "Equity for player {}: {}% ({}% draw)", 
+            player,
+            win_perc,
+            draw_perc
+        );
     }
-    println!("Out of a total of {} runs", NUM_SIMULATIONS);
-    println!("{} wins for player 0", player_win_counts[0]);
-    println!("{} wins for player 1", player_win_counts[1]);
-    println!("{} draws", NUM_SIMULATIONS - player_win_counts[0] - player_win_counts[1]);
-    println!("Equity for player 1: {}", player_win_counts[0] as f64 / NUM_SIMULATIONS as f64);
-    println!("Equity for player 2: {}", player_win_counts[1] as f64 / NUM_SIMULATIONS as f64);
 }
